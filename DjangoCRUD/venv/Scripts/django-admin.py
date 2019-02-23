a87586264e2269c789f9e62d3e91a50c195ec963
Dawwie <dawid.wietrzych@gmail.com>
2019-02-23 12:00:04 +0100

    CRUD app in Django (clear html)

---
+++ DjangoCRUD/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Pobrane\Programowanie\Pycharm 2018.3.4\Projects\DjangoCRUD\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
